4a5cbba1077f0d48f9a4a89cb05b7c83eb0a020c
FazalDevs <mohdfazalrm786@gmail.com>
2025-02-22 11:28:52 +0530

    final changes

--- frontend/vite-project/src/components/CarpoolForm.jsx
+++ frontend/vite-project/src/components/CarpoolForm.jsx
@@ -71,12 +71,12 @@ const AddressForm = () => {
         }
     };
     const customIcon = new L.Icon({
-        iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+        iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+        iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+        shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
-        shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-        shadowSize: [41, 41]
     });
 
     const handleSubmit = async (e) => {
